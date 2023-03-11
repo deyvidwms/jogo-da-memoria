89631ca087f0bfba4f67acc4614253819cb7330e
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2023-03-11 08:58:38 -0300

    Css e animação de carta virando pra cima

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,55 @@
 "use strict";
 
-function inserirCartoes() {
-  const EMOJIS = [
-    '😀', '😃', '😄', '😁', '😆', '😅', '😂', '☺️', '😊', '😇', '🙃', '😉', '😌', '😍', '😘',
-    '😗', '😙', '😚', '😋', '😛', '😝', '😜', '😎', '😏', '😒', '😞', '😔', '😟', '😕', '☹️',
-    '😣', '😖', '😫', '😩', '😢', '😭', '😠', '😡', '😳', '😱', '😨', '😰', '😥', '😓', '😶',
-    '😐', '😑', '😯', '😦', '😧', '😮', '😲', '😴', '😪', '😵', '😷', '💩', '☠️', '👽', '🤖',
-  ];
-  const LOCAL_CARTOES = document.getElementById('localCartoes');
+const EMOJIS = [
+  '😀', '😃', '😄', '😁', '😆', '😅', '😂', '☺️', '😊', '😇', '🙃', '😉', '😌', '😍', '😘',
+  '😗', '😙', '😚', '😋', '😛', '😝', '😜', '😎', '😏', '😒', '😞', '😔', '😟', '😕', '☹️',
+  '😣', '😖', '😫', '😩', '😢', '😭', '😠', '😡', '😳', '😱', '😨', '😰', '😥', '😓', '😶',
+  '😐', '😑', '😯', '😦', '😧', '😮', '😲', '😴', '😪', '😵', '😷', '💩', '☠️', '👽', '🤖',
+];
 
-  for (const emoji of EMOJIS) {
-    var div = document.createElement('div');
-    div.innerText = emoji;
-    div.className = 'cartao';
+function novoCartao(texto, status) {
+  return { texto, status };
+}
 
-    LOCAL_CARTOES.appendChild(div);
+function desvirar(cartoes) {
+  for (const cartao of cartoes) {
+    cartao.status = false;
   }
 }
 
-inserirCartoes();
+function virarCarta(cartoes, index) {
+  let cartao = cartoes[index];
+  if (cartao.status == "oculto") {
+    cartao.status = "virando";
+  } else {
+    cartao.status = "oculto"
+  }
+
+  printarCartoes(cartoes);
+}
+
+function printarCartoes(cartoes) {
+  let localCartoes = document.getElementById('localCartoes');
+  localCartoes.innerHTML = "";
+
+  cartoes.forEach((cartao, index) => {
+    let div = document.createElement('div');
+    div.id = `${index}`;
+
+    div.onclick = () => { virarCarta(cartoes, index) };
+
+    div.className = `cartao ${cartao.status}`;
+    if (cartao.status != "oculto") {
+      div.innerText = cartao.texto;
+    }
+
+    localCartoes.appendChild(div);
+
+    if (cartao.status == "virando") {
+      cartao.status = "mostrando";
+    }
+  });
+}
+
+let cartoes = EMOJIS.map((emoji) => novoCartao(emoji, "oculto"));
+printarCartoes(cartoes);
