2db278f2f680dfc8c13c20750a3a9ad1ce73a872
Deyvid William <deyvidwms@gmail.com>
2023-03-11 14:05:17 -0300

    Duplicando opções de selecionar e exibindo de forma aleatoria
    Co-authored-by: Nathãn Lima <nathan2001lima@gmail.com>
    Co-authored-by: Djavan  <zarkrai93@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,45 @@ window.onload = () => {
 
 const init = () => {
   const EMOJIS = [
-    '😀', '😃', '😄', '😁', '😆', '😅', '😂', '☺️', '😊', '😇', '🙃', '😉', '😌', '😍', '😘',
-    '😗', '😙', '😚', '😋', '😛', '😝', '😜', '😎', '😏', '😒', '😞', '😔', '😟', '😕', '☹️',
-    '😣', '😖', '😫', '😩', '😢', '😭', '😠', '😡', '😳', '😱', '😨', '😰', '😥', '😓', '😶',
-    '😐', '😑', '😯', '😦', '😧', '😮', '😲', '😴', '😪', '😵', '😷', '💩', '☠️', '👽', '🤖',
+    '😀' , '😃' , '😄' , '😁' , '😆' , '😅' , '😂' , '☺️' , '😊' , '😇' , '🙃' , '😉' , '😌' , '😍' , '😘' ,
+    '😗' , '😙' , '😚' , '😋' , '😛' , '😝' , '😜' , '😎' , '😏' , '😒' , '😞' , '😔' , '😟' , '😕' , '☹️' ,
+    '😣' , '😖' , '😫' , '😩' , '😢' , '😭' , '😠' , '😡' , '😳' , '😱' , '😨' , '😰' , '😥' , '😓' , '😶' ,
+    '😐' , '😑' , '😯' , '😦' , '😧' , '😮' , '😲' , '😴' , '😪' , '😵' , '😷' , '💩' , '☠️' , '👽' , '🤖' ,
   ];
 
   showCards(EMOJIS);
 }
 
-const showCards = (emojis) => {
-  const cardLocations = document.getElementById('cardLocations');
-  emojis.forEach( element => cardLocations.innerHTML += generateCard(element) + generateCard(element));
+function setupCards(icons) {
+  return icons.map((icon, index) => ({icon, index}));
 }
 
-const generateCard = (icon) => {
+const showCards = (emojis) => {
+  const cardLocations = document.getElementById('cardLocations');
+  
+  const emojisWithId = setupCards(emojis);
+  const duplicatedEmojis = [...emojisWithId, ...emojisWithId];
+
+  const cards = shuffle(duplicatedEmojis).map(generateCard);
+  
+  cards.forEach(card => { cardLocations.innerHTML += card });
+}
+
+function shuffle(array) {
+  return array.map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+}
+
+const generateCard = (element) => {
   const reponseElm = `
-    <div class="flip-card" data-active="off" onClick="handleClick(this)">
+    <div class="flip-card" data-index="${element.index}" data-active="on" onClick="handleClick(this)">
       <div class="flip-card--inner">
         <div class="flip-card--front">
-          <img src="./assets/images/unknow.png" alt="Imagem Interrogação">
+          <img src="./assets/images/unknow.png" alt="Verso do cartão com uma interrogação">
         </div>
         <div class="flip-card--back">
-          <p class="flip-card--icon">${icon}</p>
+          <p class="flip-card--icon">${element.icon}</p>
         </div>
       </div>
     </div>
@@ -35,9 +51,9 @@ const generateCard = (icon) => {
   return reponseElm;
 }
 
-const handleClick = ( element ) => {
-  const isActive = element.getAttribute('data-active') === "on"; 
-  element.setAttribute('data-active', isActive ? "off" : "on");
+const handleClick = (element) => {
+  const isActive = element.getAttribute('data-active') === "on";
+  element.setAttribute('data-active' , isActive ? "off" : "on");
 
   // TODO: colocar verificação se é igual ao anterior
 
